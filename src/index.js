faffae60a894df6c925aee2286ef6f435a71c19a
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-11-30 11:02:53 -0300

    Made the 1st task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,19 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile(path.join(__dirname, 'talker.json'), 'utf8');
+  const talker = JSON.parse(data);
+
+  if (!talker) {
+    res.status(200);
+    res.send([]);
+  }
+
+  res.status(200);
+  res.send(talker);
 });
 
 app.listen(PORT, () => {
