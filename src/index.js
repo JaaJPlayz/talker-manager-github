0e35f53dd99ad91058e83e10f7ea6e645a67ef19
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-11-30 11:06:55 -0300

    Made the 2nd task

--- src/index.js
+++ src/index.js
@@ -26,6 +26,27 @@ app.get('/talker', async (_req, res) => {
   res.send(talker);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await fs.readFile(path.join(__dirname, 'talker.json'), 'utf8');
+  const talker = JSON.parse(data);
+
+  if (!talker) {
+    res.status(200);
+    res.send([]);
+  }
+
+  const talkerById = talker.find((t) => t.id === Number(id));
+
+  if (!talkerById) {
+    res.status(404);
+    res.send({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200);
+  res.send(talkerById);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
